8938984ca8cacf2426d9a50c4ea6e080819bc2cd
schmelto <tom.schmelzer@web.de>
2021-03-20 11:57:20 +0100

    finally add user search

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -2,12 +2,22 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseMethods {
 
-  getUserByName(String username) async {
-    return await FirebaseFirestore.instance.collection("users").where("name", isEqualTo: username).get();
+  Future<Stream<QuerySnapshot>> getUserByUserName(String username) async {
+    return FirebaseFirestore.instance
+        .collection("users")
+        .where("name", isEqualTo: username)
+        .snapshots();
   }
+
 
   uploadUserInfo(userMap){
     FirebaseFirestore.instance.collection("users").add(userMap);
   }
+
+  // createChatRoom(String chatRoomId, Map chatRoomMap){
+  //   return FirebaseFirestore.instance.collection("chatRoom").doc(chatRoomId).set(chatRoomMap).catchError(e){
+  //     print(e.toString);
+  //   }
+  // }
 
 }
--- lib/views/search.dart
+++ lib/views/search.dart
@@ -12,45 +12,48 @@ class _SearchScreenState extends State<SearchScreen> {
   DatabaseMethods databaseMethods = new DatabaseMethods();
   TextEditingController searchEditingController = new TextEditingController();
 
-  QuerySnapshot searchSnapshot;
+  Stream usersStream;
 
   bool isLoading = false;
   bool haveUserSearched = false;
 
   initiateSearch() async {
-    if (searchEditingController.text.isEmpty) {
+    if (searchEditingController.text.isNotEmpty) {
       setState(() {
         isLoading = true;
       });
     }
-    await databaseMethods
-        .getUserByName(searchEditingController.text)
-        .then((value) {
-      searchSnapshot = value;
-      setState(() {
-        isLoading = false;
-        haveUserSearched = true;
-      });
+    usersStream = await databaseMethods.getUserByUserName(searchEditingController.text);
+    setState(() {
+      isLoading = false;
+      haveUserSearched = true;
     });
   }
 
-  Widget userList() {
-    return haveUserSearched
-        ? ListView.builder(
-            itemCount: searchSnapshot.docs.length,
-            itemBuilder: (context, index) {
-              return userTile(
-                searchSnapshot.docs[0].data()["username"],
-                searchSnapshot.docs[0].data()["userEmail"],
+Widget searchUsersList() {
+    return StreamBuilder(
+      stream: usersStream,
+      builder: (context, snapshot) {
+        return snapshot.hasData
+            ? ListView.builder(
+                itemCount: snapshot.data.docs.length,
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  DocumentSnapshot ds = snapshot.data.docs[index];
+                  return userTile(
+                      name: ds["name"],
+                      email: ds["email"]
+                    );
+                },
+              )
+            : Container(
+                child: CircularProgressIndicator(), // wait for input
               );
-            },
-          )
-        : Container(
-          child: Text("Text", style: simpleTextStyle(),),
-        );
+      },
+    );
   }
 
-  Widget userTile(String userName, String userEmail) {
+  Widget userTile({String name, email}) {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
       child: Row(
@@ -59,11 +62,11 @@ class _SearchScreenState extends State<SearchScreen> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
-                userName,
+                name,
                 style: TextStyle(color: Colors.white, fontSize: 16),
               ),
               Text(
-                userEmail,
+                email,
                 style: TextStyle(color: Colors.white, fontSize: 16),
               )
             ],
@@ -88,6 +91,26 @@ class _SearchScreenState extends State<SearchScreen> {
     );
   }
 
+    sendMessage(String userName){
+    // List<String> users = [Constants.myName,userName];
+
+    // String chatRoomId = getChatRoomId(Constants.myName,userName);
+
+    // Map<String, dynamic> chatRoom = {
+    //   "users": users,
+    //   "chatRoomId" : chatRoomId,
+    // };
+
+    // databaseMethods.addChatRoom(chatRoom, chatRoomId);
+
+    // Navigator.push(context, MaterialPageRoute(
+    //   builder: (context) => Chat(
+    //     chatRoomId: chatRoomId,
+    //   )
+    // ));
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -102,12 +125,14 @@ class _SearchScreenState extends State<SearchScreen> {
                 child: Column(
                   children: [
                     Container(
-                      // padding:
-                      //     EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 16, vertical: 16),
                       child: Row(
                         children: [
                           Expanded(
                             child: TextField(
+                              textInputAction: TextInputAction.go,
+                              onSubmitted: (value) => {initiateSearch()},
                               controller: searchEditingController,
                               style: simpleTextStyle(),
                               decoration: InputDecoration(
@@ -119,7 +144,7 @@ class _SearchScreenState extends State<SearchScreen> {
                                   suffixIcon: IconButton(
                                       onPressed: () => {
                                             initiateSearch()
-                                          }, //TODO: on pressed
+                                          },
                                       icon: Icon(Icons.search,
                                           color: Colors.white))),
                             ),
@@ -127,7 +152,8 @@ class _SearchScreenState extends State<SearchScreen> {
                         ],
                       ),
                     ),
-                    userList()
+                    // userList()
+                    searchUsersList()
                   ],
                 ),
               ));
