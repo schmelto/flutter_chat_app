84fdb0808da3fb5c8bf627af28aee080725ead07
schmelto <tom.schmelzer@web.de>
2021-03-28 22:53:49 +0200

    add google sign in method

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,6 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_chat_app/helper/helperfunctions.dart';
 import 'package:flutter_chat_app/model/chatuser.dart';
+import 'package:flutter_chat_app/views/conversationScreen.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -34,6 +38,27 @@ class AuthMethods {
     } catch (e) {
       print(e.toString());
       return null;
+    }
+  }
+
+  Future<User> signInWithGoogle(BuildContext context) async {
+    final GoogleSignIn _googleSignIn = new GoogleSignIn();
+
+    final GoogleSignInAccount googleSignInAccount =
+        await _googleSignIn.signIn();
+    final GoogleSignInAuthentication googleSignInAuthentication =
+        await googleSignInAccount.authentication;
+
+    final AuthCredential credential = GoogleAuthProvider.credential(
+        idToken: googleSignInAuthentication.idToken,
+        accessToken: googleSignInAuthentication.accessToken);
+
+    UserCredential result = await _auth.signInWithCredential(credential);
+    User userDetails = result.user;
+
+    if (result == null) {
+    } else {
+      Navigator.push(context, MaterialPageRoute(builder: (context) => Chat()));
     }
   }
 
